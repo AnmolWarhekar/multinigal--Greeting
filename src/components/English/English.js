ee0e7c2142b5d86916777c0f2f97856d7392a5cf
AnmolWarhekar <warhekaranmol@gmail.com>
2023-02-23 10:37:21 +0000

    first comment

---
+++ src/components/English/English.js
@@ -0,0 +1,13 @@
+import './english.css'
+
+const English = () => (
+  <div className="align">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/multilingual_greeting/english-greetings-img.png"
+      alt="english"
+      className="alignImage"
+    />
+  </div>
+)
+
+export default English
